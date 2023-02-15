eb86de6c86fda3a3aa99a4b365a91c3807a9cc67
XiaoMouz <gxiaomouz@gmail.com>
2023-02-15 16:29:45 +0800

    feat: login system work right now

--- src/main/java/com/jxcia202/jspdemo1/bean/account/LoginBean.java
+++ src/main/java/com/jxcia202/jspdemo1/bean/account/LoginBean.java
@@ -3,4 +3,9 @@ package com.jxcia202.jspdemo1.bean.account;
 public class LoginBean {
     public String username;
     public String password;
+
+    @Override
+    public String toString() {
+        return "username:"+username+"|password:"+password;
+    }
 }
--- src/main/java/com/jxcia202/jspdemo1/controller/UserController.java
+++ src/main/java/com/jxcia202/jspdemo1/controller/UserController.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.util.*;
 
 import com.jxcia202.jspdemo1.bean.User;
+import com.jxcia202.jspdemo1.bean.account.LoginBean;
 import com.jxcia202.jspdemo1.bean.users.Administrator;
 import com.jxcia202.jspdemo1.bean.users.Editor;
 import com.jxcia202.jspdemo1.bean.users.Maintainer;
@@ -21,61 +22,60 @@ import com.sun.tools.javac.Main;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 public class UserController {
-
+    Connection remote = ConnectionFactory.getConnection();
     private Map<String, User> userDatabase = new HashMap<>() {
         {
             try{
                 ArrayList<User> onlineUsers = new ArrayList<User>();
-                Connection remote = ConnectionFactory.getConnection();
                 PreparedStatement statement = remote.prepareStatement("select * from user");
                 ResultSet resultSet = statement.executeQuery();
                 while(resultSet.next()){
                         switch(resultSet.getString("userlevel")){
-                            case "ADMINISTRATOR": onlineUsers.add(new Administrator(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
-                            case "EDITOR": onlineUsers.add(new Editor(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
-                            case "MAINTAINER": onlineUsers.add(new Maintainer(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
-                            case "READER": onlineUsers.add(new Reader(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            case "ADMINISTRATOR": onlineUsers.add(new Administrator(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("password"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            case "EDITOR": onlineUsers.add(new Editor(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("password"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            case "MAINTAINER": onlineUsers.add(new Maintainer(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("password"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            case "READER": onlineUsers.add(new Reader(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("password"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
                             default: break;
                         }
                 }
                 onlineUsers.forEach(user -> {put(user.getUsername(),user);});
             }catch (Exception e){
                 List<User> offlineUsers = List.of(
-                        new Administrator(1,"XiaoMouz","gxiaomouz@gmail.com","114514","127.0.0.1","127.0.0.1",new Date(),new Date()),
-                        new Editor(2,"test","test@gmail.com","11451419","127.0.0.1","127.0.0.1",new Date(),new Date()),
-                        new Reader(3,"a reader","reader@gmail.com","1919810","127.0.0.1","127.0.0.1",new Date(),new Date())
+                        new Administrator(1,"XiaoMouz","123000","gxiaomouz@gmail.com","114514","127.0.0.1","127.0.0.1",new Date(),new Date()),
+                        new Editor(2,"test", "woshinidie","test@gmail.com","11451419","127.0.0.1","127.0.0.1",new Date(),new Date()),
+                        new Reader(3,"a reader","nimeimama","reader@gmail.com","1919810","127.0.0.1","127.0.0.1",new Date(),new Date())
                 );
                 offlineUsers.forEach(user -> {put(user.getUsername(),user);});
             }
         }
+
     };
 
-    @GetMapping("/signin")
-    public ModelAndView signin() {
-        return new ModelAndView("/signin.html");
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("/login.html");
     }
 
-    // todo: "sign in logic"
-//    @PostMapping("/signin")
-//    public ModelAndView doSignin(SignInBean bean, HttpServletResponse response, HttpSession session) throws IOException {
-//        User user = userDatabase.get(bean.email);
-//        if (user == null || !user.password.equals(bean.password)) {
-//            response.setContentType("application/json");
-//            PrintWriter pw = response.getWriter();
-//            pw.write("{\"error\":\"Bad email or password\"}");
-//            pw.flush();
-//        } else {
-//            session.setAttribute("user", user);
-//            response.setContentType("application/json");
-//            PrintWriter pw = response.getWriter();
-//            pw.write("{\"result\":true}");
-//            pw.flush();
-//        }
-//        return null;
-//    }
+    @PostMapping("/login")
+    public ModelAndView doLogin(LoginBean bean, HttpServletResponse response, HttpSession session) throws IOException {
+        User user = userDatabase.get(bean.username);
+        if (user == null || !user.getPassword().equals(bean.password)) {
+            response.setContentType("application/json");
+            PrintWriter pw = response.getWriter();
+            pw.write("{\"error\":\"Bad email or password\"}");
+            pw.flush();
+        } else {
+            session.setAttribute("user", user);
+            response.setContentType("application/json");
+            PrintWriter pw = response.getWriter();
+            pw.write("{\"result\":true}");
+            pw.flush();
+        }
+        return null;
+    }
 
-    @GetMapping("/signout")
-    public ModelAndView signout(HttpSession session) {
+    @GetMapping("/logout")
+    public ModelAndView logout(HttpSession session) {
         session.removeAttribute("user");
         return new ModelAndView("redirect:/");
     }
