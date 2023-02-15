2fe4e01faff514364ddbcc6d34d85c6d5331cc37
XiaoMouz <gxiaomouz@gmail.com>
2023-02-15 15:32:21 +0800

    feat: get remote database user list and default user list

--- src/main/java/com/jxcia202/jspdemo1/controller/UserController.java
+++ src/main/java/com/jxcia202/jspdemo1/controller/UserController.java
@@ -2,26 +2,51 @@ package com.jxcia202.jspdemo1.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.*;
 
 import com.jxcia202.jspdemo1.bean.User;
+import com.jxcia202.jspdemo1.bean.users.Administrator;
+import com.jxcia202.jspdemo1.bean.users.Editor;
+import com.jxcia202.jspdemo1.bean.users.Maintainer;
+import com.jxcia202.jspdemo1.bean.users.Reader;
 import com.jxcia202.jspdemo1.framework.GetMapping;
 import com.jxcia202.jspdemo1.framework.ModelAndView;
 import com.jxcia202.jspdemo1.framework.PostMapping;
+import com.jxcia202.jspdemo1.jdbc.ConnectionFactory;
+import com.sun.tools.javac.Main;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 public class UserController {
 
     private Map<String, User> userDatabase = new HashMap<>() {
         {
-//            List<User> users = List.of( //
-//                    new User("bob@example.com", "bob123", "Bob", "This is bob."), new User("tom@example.com", "tomcat", "Tom", "This is tom."));
-//            users.forEach(user -> {
-//                put(user.email, user);
-//            });
-            // todo: "get user from database"
+            try{
+                ArrayList<User> onlineUsers = new ArrayList<User>();
+                Connection remote = ConnectionFactory.getConnection();
+                PreparedStatement statement = remote.prepareStatement("select * from user");
+                ResultSet resultSet = statement.executeQuery();
+                while(resultSet.next()){
+                        switch(resultSet.getString("userlevel")){
+                            case "ADMINISTRATOR": onlineUsers.add(new Administrator(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            case "EDITOR": onlineUsers.add(new Editor(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            case "MAINTAINER": onlineUsers.add(new Maintainer(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            case "READER": onlineUsers.add(new Reader(resultSet.getInt("id"),resultSet.getString("username"),resultSet.getString("email"),resultSet.getString("token"),resultSet.getString("registerIp"),resultSet.getString("lastLoginIp"),resultSet.getDate("lastLoginTime"),resultSet.getDate("registerTime")));break;
+                            default: break;
+                        }
+                }
+                onlineUsers.forEach(user -> {put(user.getUsername(),user);});
+            }catch (Exception e){
+                List<User> offlineUsers = List.of(
+                        new Administrator(1,"XiaoMouz","gxiaomouz@gmail.com","114514","127.0.0.1","127.0.0.1",new Date(),new Date()),
+                        new Editor(2,"test","test@gmail.com","11451419","127.0.0.1","127.0.0.1",new Date(),new Date()),
+                        new Reader(3,"a reader","reader@gmail.com","1919810","127.0.0.1","127.0.0.1",new Date(),new Date())
+                );
+                offlineUsers.forEach(user -> {put(user.getUsername(),user);});
+            }
         }
     };
 
